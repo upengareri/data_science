23299e2821a3eda397a7c4e3a678f80a31bd5cbb
Upendar Gareri <u.gareri@gmail.com>
2020-04-07 22:08:05 +0200

    Add learning exercises from PLYMI: part2

--- notes/PLYMI.py
+++ notes/PLYMI.py
@@ -7,6 +7,8 @@ def merge_max_mappings(*dicts):
 
     return merged 
 
+# -------------------------------------END-------------------------------------------
+
 def is_palindrome(input_str):
     """ Given a string, determine if it is palindrome or not.
     Whitespace, non alpha-numeric and character casing are all ignored
@@ -22,6 +24,8 @@ def is_palindrome(input_str):
     filtered_str = "".join(c for c in input_str.lower() if c.isalnum())
     return filtered_str == filtered_str[::-1]
     
+# --------------------------------------END------------------------------------------
+
 def difference_fanout(nums, fanout):
     """Return a list of differences for each value with its following fans
 
@@ -54,6 +58,28 @@ def difference_fanout(nums, fanout):
     res = [[neighbour - num for neighbour in nums[i+1:i+1+fanout]] for i, num in enumerate(nums)]
     return res    
 
+# ------------------------------------END--------------------------------------------
+
+def int_to_word(num):
+    """ Takes an integer and formats it into a special string
+        e.g. 142 -> one-four-two
+             -12 -> neg-one-two
+    """
+    mapping = {"0": "zero", "1": "one", "2": "two", "3": "three",
+               "4": "four", "5": "five", "6": "six", "7": "seven",
+               "8": "eight", "9": "nine", "-": "neg"}
+    
+    return "-".join(mapping[digit] for digit in str(num))
+
+def input_to_transcript(item):
+    """ Any -> str"""
+    if isinstance(item, bool): return "<Other>"  # in-line syntax
+    if isinstance(item, int): return int_to_word(item)
+    if isinstance(item, float): return int_to_word(int(item)) + " and float"
+    if isinstance(item, str): return item
+    return "<Other>"
+
+
 def concat_to_str(l):
     """Maps a list of object in the list to its string representation concatenated together
 
@@ -72,7 +98,9 @@ def concat_to_str(l):
     >>> concat_to_str([1, None, "hi", 2.0])
     one | <Other> | hi | two and float
     """
+    return " | ".join(input_to_transcript(item) for item in l)
 
+# --------------------------------------END------------------------------------------
 
 if __name__ == "__main__":
     a = dict(a=0, b=100, c=3)
@@ -91,3 +119,9 @@ if __name__ == "__main__":
 
     res = difference_fanout([3, 2, 4, 6, 1], 3)
     print("Fanout: ", res)
+
+    l = [12,-14.23,"hello", True, "Aha", 10.1, None, 5]
+    s = concat_to_str(l)
+    print("Encode as string:")
+    print("Input: ", l)
+    print("Output: ", s)
